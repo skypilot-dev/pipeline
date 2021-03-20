f247641b8e322f4eb305f00c45b35e8b2e60166d
William Thorsen <william@thorsen.dev>
2021-03-20 13:44:45 -0700

    tooling: Install & configure Toolchain

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  moduleFileExtensions: [
+    'ts',
+    'js',
+    'json',
+  ],
+  moduleNameMapper: {
+    'root/(.*)$': '<rootDir>/$1',
+    'src/(.*)$': '<rootDir>/src/$1',
+  },
+  testRegex: '__tests__/.*\\.test\\.ts$',
+  /* Define preprocessors */
+  transform: {
+    '^.+\\.ts$': 'babel-jest',
+  },
+  verbose: false,
+};
---
+++ jest.standalone.config.js
@@ -0,0 +1,6 @@
+const baseConfig = require('./jest.config');
+
+module.exports = {
+  ...baseConfig,
+  testRegex: '__tests__/.*\\.(app|comp|unit)\\.test\\.ts$',
+};
