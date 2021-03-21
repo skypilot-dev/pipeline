5279f745e474633ce3bfce30e2126fe02bae47b8
William Thorsen <william@thorsen.dev>
2021-03-20 18:18:49 -0700

    refactor: Remove Pipeline-supplied options from StepParams

--- src/core/Step.ts
+++ src/core/Step.ts
@@ -3,16 +3,20 @@ import type { Pipeline } from './Pipeline';
 
 type Handler<Context> = ((context?: Partial<Context>) => MaybePromise<Partial<Context> | void>);
 
+interface PipelineStepParams<Context> {
+  pipeline: Pipeline<Context>;
+}
+
 export interface StepParams<Context> {
   handle: Handler<Context>;
-  pipeline: Pipeline<Context>;
+  name?: string;
 }
 
 export class Step<Context extends Dict> {
   private readonly handle: Handler<Context>;
   private readonly pipeline: Pipeline<Context>;
 
-  constructor(stepParams: StepParams<Context>) {
+  constructor(stepParams: StepParams<Context> & PipelineStepParams<Context>) {
     const { pipeline, handle } = stepParams;
 
     this.handle = handle || (context => context);
